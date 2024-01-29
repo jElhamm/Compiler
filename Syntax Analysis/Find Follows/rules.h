ad5b453338df6113f87c8fef267aebd99d46c05b
jelhamm <elhamjafari200312@gmail.com>
2024-01-30 01:23:49 +0330

    Defines a struct called Rule, which represents a production rule

---
+++ Syntax Analysis/Find Follows/rules.h
@@ -0,0 +1,28 @@
+// The code you provided is a header file named "rules.h". This header file defines a structure called Rule,
+// which represents a production rule. Each Rule consists of a non-terminal symbol and an array of symbols.
+
+
+
+#ifndef RULES_H
+#define RULES_H
+#define MAX_RULES 10
+#define MAX_SYMBOLS 10
+
+
+typedef struct {
+    char nonTerminal;
+    char symbols[MAX_SYMBOLS];
+    int symbolCount;
+} Rule;
+
+#endif
+
+
+
+/**
+ * The Rule struct has the following members:
+ * 
+ * 'nonTerminal': a character representing the non-terminal symbol of the production rule.
+ * 'symbols'    : an array of characters representing the symbols (terminals or non-terminals) in the production rule.
+ * 'symbolCount': an integer indicating the number of symbols in the production rule.
+**/
